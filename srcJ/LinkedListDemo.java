ecc2277792c5f83013448ef3913da48b397482f2
JeffreyCoxChatham <jeffrey.cox@chatham.edu>
2025-02-24 15:01:56 -0500

    Added read and write tests
    Added read and write tests to collection demos. Since linked list tests can take very long, added abort feature that aborts tests after 1 minute, and informs the user.

--- srcJ/LinkedListDemo.java
+++ srcJ/LinkedListDemo.java
@@ -6,16 +6,50 @@ public class LinkedListDemo {
 
     public LinkedListDemo(int howManyNums, Random rand) {
         long start = System.currentTimeMillis();
+        boolean accuracy = true;
 
         long fstart = System.currentTimeMillis();
         nums = new LinkedList<Integer>();
         for(int i = 0; i < howManyNums; i++) {
             nums.add(rand.nextInt(howManyNums));
+            if (System.currentTimeMillis() - fstart > 60000) {
+                System.out.println(String.format("LinkedList Fill Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
         }
         long fend = System.currentTimeMillis();
         System.out.println(String.format("LinkedList Fill Time: %.3f seconds", (fend - fstart) / 1000.0));
 
+        long rstart = System.currentTimeMillis();
+        float readDummy;
+        for(int i = 0; i < howManyNums; i++) {
+            readDummy = nums.get(i);
+            if (System.currentTimeMillis() - rstart > 60000) {
+                System.out.println(String.format("LinkedList Read Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
+        }
+        long rend = System.currentTimeMillis();
+        System.out.println(String.format("LinkedList Read Time: %.3f seconds", (rend - rstart) / 1000.0));
+
+        long wstart = System.currentTimeMillis();
+        for(int i = 0; i < howManyNums; i++) {
+            nums.set(i, rand.nextInt(howManyNums));
+            if (System.currentTimeMillis() - wstart > 60000) {
+                System.out.println(String.format("LinkedList Write Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
+        }
+        long wend = System.currentTimeMillis();
+        System.out.println(String.format("LinkedList Write Time: %.3f seconds", (wend - wstart) / 1000.0));
+
         long end = System.currentTimeMillis();
         System.out.println(String.format("LinkedList Total Time: %.3f seconds", (end - start) / 1000.0));
+        if (!accuracy) {
+            System.out.println("Times may be inaccurate due to test abortion.");
+        }
     }
 }
--- srcJ/VectorDemo.java
+++ srcJ/VectorDemo.java
@@ -6,16 +6,50 @@ public class VectorDemo {
 
     public VectorDemo(int howManyNums, Random rand) {
         long start = System.currentTimeMillis();
+        boolean accuracy = true;
 
         long fstart = System.currentTimeMillis();
         nums = new Vector<Integer>();
         for(int i = 0; i < howManyNums; i++) {
             nums.add(rand.nextInt(howManyNums));
+            if (System.currentTimeMillis() - fstart > 60000) {
+                System.out.println(String.format("Vector Fill Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
         }
         long  fend = System.currentTimeMillis();
         System.out.println(String.format("Vector Fill Time: %.3f seconds", (fend - fstart) / 1000.0));
 
+        long rstart = System.currentTimeMillis();
+        float readDummy;
+        for(int i = 0; i < howManyNums; i++) {
+            readDummy = nums.get(i);
+            if (System.currentTimeMillis() - rstart > 60000) {
+                System.out.println(String.format("Vector Read Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
+        }
+        long rend = System.currentTimeMillis();
+        System.out.println(String.format("Vector Read Time: %.3f seconds", (rend - rstart) / 1000.0));
+
+        long wstart = System.currentTimeMillis();
+        for(int i = 0; i < howManyNums; i++) {
+            nums.setElementAt(i, rand.nextInt(howManyNums));
+            if (System.currentTimeMillis() - wstart > 60000) {
+                System.out.println(String.format("Vector Write Test aborted at 1 minute. Current index: %d out of %d.", i, howManyNums));
+                accuracy = false;
+                break;
+            }
+        }
+        long wend = System.currentTimeMillis();
+        System.out.println(String.format("Vector Write Time: %.3f seconds", (wend - wstart) / 1000.0));
+
         long end = System.currentTimeMillis();
         System.out.println(String.format("Vector Total Time: %.3f seconds", (end - start) / 1000.0));
+        if (!accuracy) {
+            System.out.print("Times may be inaccurate due to test abortion.");
+        }
     }
 }
